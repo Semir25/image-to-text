08c692a2b0b8788ddc8a81930fef4157c42d92c4
Semir <bsemir54@gmail.com>
2022-04-17 23:05:28 +0200

    added gradient option and some examples

--- transform.py
+++ transform.py
@@ -3,15 +3,19 @@ import numpy
 
 image_path = input('Enter the path to the image (image.jpg): ')
 
-image_name = image_path.split('/')
-image_name = image_name[len(image_name) - 1]
-image_extension = image_name.split('.')
-image_extension = image_extension[len(image_extension) - 1]
-if image_extension != 'jpg':
+image_name = image_path.strip().split('/')
+image_name = image_name[len(image_name) - 1].split('.')
+image_extension = image_name[len(image_name) - 1]
+image_name = image_name[0]
+if image_extension != 'jpg' and image_extension != 'jpeg' and image_extension != 'png':
     raise Exception('not a valid format!')
 
 
 char_width = input('Enter width of output file (1 = 1 characte): ')
+palette = input('Enter chatacters for palette/gradient (leave blank for default palette): ')
+
+if len(palette) == 0:
+    palette = '@@@@###$?//--_______'
 
 im = Image.open(image_path, 'r')
 width, height = im.size
@@ -19,7 +23,7 @@ pixel_values = list(im.getdata())
 pixel_values = numpy.array(pixel_values).reshape((width, height, 3))
 
 
-f = open(image_name.replace('jpg','txt'), "w")
+f = open(image_name+'.txt', "w")
 
 blur_width = int(width/int(char_width))
 blur_height = blur_width*2
@@ -32,24 +36,25 @@ for i in range(0, h, blur_height):
         tmp = 0
         for x in range(blur_height):
             for y in range(blur_width):
-                #if i+y < width and j+x < height:
-                tmp += pixel_values[i+y][j+x][0] + pixel_values[i+y][j+x][1] + pixel_values[i+y][j+x][2]
+                if i+y < width and j+x < height:
+                    tmp += pixel_values[i+y][j+x][0] + pixel_values[i+y][j+x][1] + pixel_values[i+y][j+x][2]
         tmp /= blur_width*blur_height*3
-        tmp = int(tmp)
-        if tmp < 80:
-            f.write('#')
-        elif tmp < 90:
-            f.write('@')
-        elif tmp < 100:
-            f.write('$')
-        elif tmp < 110:
-            f.write('u')
-        elif tmp < 120:
-            f.write('o')
-        elif tmp < 150:
-            f.write('/')
-        else:
-            f.write('_')
+        tmp = int(tmp/(256/len(palette)))
+        f.write(palette[tmp])
+        # if tmp < 50:
+        #     f.write('#')
+        # elif tmp < 90:
+        #     f.write('@')
+        # elif tmp < 120:
+        #     f.write('$')
+        # elif tmp < 130:
+        #     f.write('?')
+        # elif tmp < 145:
+        #     f.write('/')
+        # elif tmp < 165:
+        #     f.write('-')
+        # else:
+        #     f.write('_')
     f.write("\n")
 f.close()
 im.close()
